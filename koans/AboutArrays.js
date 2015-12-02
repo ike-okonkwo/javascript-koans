5bee9a37820a7f1a404a6f596cd50e4693843c07
Ike Okonkwo <paschal83@gmail.com>
2015-12-02 00:16:18 -0800

    Still have one test remaining in both AboutExpects.js and AboutArrays.js

--- koans/AboutArrays.js
+++ koans/AboutArrays.js
@@ -9,8 +9,8 @@ describe("About Arrays", function() {
     var multiTypeArray = [0, 1, "two", function () { return 3; }, {value1: 4, value2: 5}, [6, 7]];
     expect(multiTypeArray[0]).toBe(0);
     expect(multiTypeArray[2]).toBe('two');
-    expect(multiTypeArray[3]()).toBe(function () { return 3; });
-    expect(multiTypeArray[4].value1).toBe( {value1: 4, value2: 5});
+    expect(multiTypeArray[3]()).toBe(3);
+    expect(multiTypeArray[4].value1).toBe(4);
     expect(multiTypeArray[4]["value2"]).toBe(5);
     expect(multiTypeArray[5][0]).toBe(6);
   });
@@ -50,7 +50,7 @@ describe("About Arrays", function() {
     expect(array.slice(0, 2)).toEqual(["peanut","butter"]);
     expect(array.slice(2, 2)).toEqual([]);
     expect(array.slice(2, 20)).toEqual([ "and", "jelly"]);
-    expect(array.slice(3, 0)).toEqual([]]);
+    expect(array.slice(3, 0)).toEqual([]);
     expect(array.slice(3, 100)).toEqual(["jelly"]);
     expect(array.slice(5, 1)).toEqual([]);
   });
@@ -62,36 +62,36 @@ describe("About Arrays", function() {
         refArray[1] = "changed in function";
     }
     passedByReference(array);
-    expect(array[1]).toBe(FILL_ME_IN);
+    expect(array[1]).toBe('one');
 
     var assignedArray = array;
     assignedArray[5] = "changed in assignedArray";
-    expect(array[5]).toBe(FILL_ME_IN);
+    expect(array[5]).toBe('changed in assignedArray');
 
     var copyOfArray = array.slice();
     copyOfArray[3] = "changed in copyOfArray";
-    expect(array[3]).toBe(FILL_ME_IN);
+    expect(array[3]).toBe('three');
   });
 
   it("should push and pop", function () {
     var array = [1, 2];
     array.push(3);
 
-    expect(array).toEqual(FILL_ME_IN);
+    expect(array).toEqual([1, 2, 3]);
 
     var poppedValue = array.pop();
-    expect(poppedValue).toBe(FILL_ME_IN);
-    expect(array).toEqual(FILL_ME_IN);
+    expect(poppedValue).toBe(3);
+    expect(array).toEqual([1, 2]);
   });
 
   it("should know about shifting arrays", function () {
     var array = [1, 2];
 
     array.unshift(3);
-    expect(array).toEqual(FILL_ME_IN);
+    expect(array).toEqual([3, 1, 2]);
 
     var shiftedValue = array.shift();
-    expect(shiftedValue).toEqual(FILL_ME_IN);
-    expect(array).toEqual(FILL_ME_IN);
+    expect(shiftedValue).toEqual(3);
+    expect(array).toEqual([1, 2]);
   });
 });
